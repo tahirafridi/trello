ee8362557d7205963790e27874fd3ac10b767bbb
tahirafridi <tahirkhanafridi@gmail.com>
2023-01-08 23:29:39 +0500

    Update App.vue

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -22,51 +22,5 @@
                 </div>
             </div>
         </div>
-
-        <div class="board">
-            <div class="board__header">
-                <span>Board No. 1</span>
-                <button class="btn btn--delete">Delete</button>
-            </div>
-            <div class="board__body">
-                <div class="card">
-                    <div class="card__header">Card No. 1</div>
-                    <div class="card__body">Card Body</div>
-                </div>
-
-                <div class="card">
-                    <div class="card__header">Card No. 1</div>
-                    <div class="card__body">Card Body</div>
-                </div>
-
-                <div class="card">
-                    <div class="card__header">Card No. 1</div>
-                    <div class="card__body">Card Body</div>
-                </div>
-            </div>
-        </div>
-
-        <div class="board">
-            <div class="board__header">
-                <span>Board No. 1</span>
-                <button class="btn btn--delete">Delete</button>
-            </div>
-            <div class="board__body">
-                <div class="card">
-                    <div class="card__header">Card No. 1</div>
-                    <div class="card__body">Card Body</div>
-                </div>
-
-                <div class="card">
-                    <div class="card__header">Card No. 1</div>
-                    <div class="card__body">Card Body</div>
-                </div>
-
-                <div class="card">
-                    <div class="card__header">Card No. 1</div>
-                    <div class="card__body">Card Body</div>
-                </div>
-            </div>
-        </div>
     </div>
 </template>
